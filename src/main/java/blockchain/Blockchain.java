ab49408759cad9bb724fa3a9b3ce7a3c6b375a60
Franek-Antoniak <31598277+Franek-Antoniak@users.noreply.github.com>
2021-07-06 21:33:52 +0200

    feat: block creation
    Long descritpon:
    encryption with sha256 algorithm
    creation of blocks via factory pattern
    adding the block class
    builder pattern
    
    BREAKING CHANGE: Create blocks, yet without mining the nonce value

---
+++ src/main/java/blockchain/Blockchain.java
@@ -0,0 +1,54 @@
+package blockchain;
+
+import blockchain.block.Block;
+import blockchain.block.BlockFactory;
+import lombok.Getter;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+/**
+ * Represents a Blockchain.
+ */
+@Getter
+public class Blockchain {
+    /* List that contains Block with info */
+    private final LinkedList<Block> blockList = new LinkedList<>();
+    private final BlockFactory blockFactory = new BlockFactory();
+
+    public void addNewBlock(Block newBlock) {
+        blockList.add(newBlock);
+    }
+
+    // FIXME: 06.07.2021 Don't know yet if it's good idea.
+    public Block getLastBlock() {
+        try {
+            return blockList.getLast();
+        } catch (NoSuchElementException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Returns a string representation of this Blockchain. The string
+     * representation consists of a list of Blocks separated by new lines.
+     * <p>Block are converted to strings by overridden method {@link Block#toString()}.</p>
+     *
+     * @return a string representation of Blockchain
+     */
+    @Override
+    public String toString() {
+        Iterator<Block> it = blockList.iterator();
+        if (!it.hasNext())
+            return "";
+        StringBuilder sb = new StringBuilder();
+        for (; ; ) {
+            Block block = it.next();
+            sb.append(block);
+            if (!it.hasNext())
+                return sb.toString();
+            sb.append('\n');
+        }
+    }
+}

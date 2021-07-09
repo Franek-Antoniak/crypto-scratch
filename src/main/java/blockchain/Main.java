c46598155dbee0a5c418a7d500019323ecbe039c
Franek-Antoniak <31598277+Franek-Antoniak@users.noreply.github.com>
2021-07-09 15:01:28 +0200

    Stage 2/6 (#7)
    * Stage 1/5 passed, now you can create blockchain, yet without mining blocks and print it by calling toString method.
    
    * fixup! Stage 1/6 passed, now you can create blockchain, yet without mining blocks and print it by calling toString method.
    
    * Adding gitignore
    
    * fixup
    
    * fixup
    
    * fixup
    
    * fixup
    
    * New tests
    
    * I add the magic number and the block generation time. The block must start with a certain number of zeros.
    
    * Some fixes, now all tests passed.
    
    * Now you can serialize Blockchain into Json file and deserialize in app.

--- src/main/java/blockchain/Main.java
+++ src/main/java/blockchain/Main.java
@@ -1,17 +1,20 @@
 package blockchain;
 
 import blockchain.miner.CryptoMiner;
+import blockchain.serializer.BlockchainJsonSerializer;
 
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        BlockchainJsonSerializer serializer = new BlockchainJsonSerializer();
         long magicNumber = getMagicNumber();
         Blockchain blockchain = new Blockchain();
         CryptoMiner cryptoMiner = new CryptoMiner(blockchain, magicNumber);
         cryptoMiner.mineBlocks(5);
         System.out.println(blockchain);
+        serializer.serialize(blockchain);
     }
 
     private static long getMagicNumber() {

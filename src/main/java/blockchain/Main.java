61de1c17abea370a12d484f4a9c31fe947ff24d7
Franek Antoniak <franekant123@gmail.com>
2022-09-11 02:34:00 +0200

    build: gradle update

--- src/main/java/blockchain/Main.java
+++ src/main/java/blockchain/Main.java
@@ -1,19 +1,12 @@
 package blockchain;
 
-import blockchain.cryptomine.controller.MineController;
-import blockchain.messenger.Messenger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.concurrent.TimeUnit;
-
+@SpringBootApplication
 public class Main {
-    private static final MineController mineController = MineController.getInstance();
-    private static final Messenger messenger = Messenger.getInstance();
-
-    public static void main(String[] args) throws InterruptedException, InvocationTargetException {
-        mineController.addNewMainers(4);
-        messenger.addNewUser();
-        TimeUnit.SECONDS.sleep(20);
-        mineController.removeAllMainers();
+    public static void main(String[] args) {
+        System.out.println("Starting application...");
+        SpringApplication.run(Main.class, args);
     }
 }
